d6afb06b5c8eb13683bfe9f770c4460af140ab31
Atef Ben Ali <atefBB@users.noreply.github.com>
2017-03-19 11:12:57 +0100

    format the code

--- myscript.js
+++ myscript.js
@@ -1,17 +1,17 @@
-
-function switchRTL(){
+function switchRTL() {
 	
 	//switch the preview
 	var objBody = jQuery(".markdown-body");
-	if(objBody.length){
+	if(objBody.length)
+	{
 		objBody.css("direction","rtl");
 		objBody.find("pre").css("direction","ltr");
 	}
 	
 	//switch the editor
 	var objEditor = jQuery(".ace_editor");
-	if(objEditor.length){
-		
+	if(objEditor.length)
+	{	
 		objEditor.css("direction","rtl");
 		jQuery("textarea.ace_text-input").css("direction","rtl");
 		jQuery(".atom-text-editor").css("direction","rtl");
@@ -22,7 +22,6 @@ function switchRTL(){
 		});
 		
 	}
-	
 }
 
 switchRTL();
