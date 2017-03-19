f4ccad1dc12489197c1b64f685327cc1e647bb48
Atef Ben Ali <atefBB@users.noreply.github.com>
2017-03-19 11:14:05 +0100

    format thr code

--- background.js
+++ background.js
@@ -1,16 +1,11 @@
-
-
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-
   chrome.tabs.executeScript({
 	file: 'jquery.js',
   });
 
  chrome.tabs.executeScript({
 	file: 'myscript.js',
-  });
-
-   
+  }); 
 });
